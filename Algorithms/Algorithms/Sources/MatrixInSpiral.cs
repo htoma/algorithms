c51f5d253d13366cfb933a97d5b935dcb82a964e
ht0ma <horia.toma@yahoo.com>
2014-04-12 01:54:21 +0200

    Early morning coding from paper

---
+++ Algorithms/Algorithms/Sources/MatrixInSpiral.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace Algorithms.Sources
+{
+    public static class MatrixInSpiral
+    {
+        public static void Traverse(int[,] matrix, int n, int m)
+        {
+            int iStart = 0;
+            int iEnd = n - 1;
+            int jStart = 0;
+            int jEnd = m - 1;
+
+            int direction = 0;
+
+            while (iStart <= iEnd && jStart <= jEnd)
+            {
+                if (direction == 0)
+                {
+                    for (int j = jStart; j <= jEnd; j++)
+                    {
+                        Console.Write("{0} ", matrix[iStart, j]);
+                    }
+                    iStart++;
+                }
+                else if (direction == 1)
+                {
+                    for (int i = iStart; i <= iEnd; i++)
+                    {
+                        Console.Write("{0} ", matrix[i, jEnd]);
+                    }
+                    jEnd--;
+                }
+                else if (direction == 2)
+                {
+                    for (int j = jEnd; j >= jStart; j--)
+                    {
+                        Console.Write("{0} ", matrix[iEnd, j]);
+                    }
+                    iEnd--;
+                }
+                else
+                {
+                    for (int i = iEnd; i >= iStart; i--)
+                    {
+                        Console.Write("{0} ", matrix[i, jStart]);
+                    }
+                    jStart++;
+                }
+                direction = ++direction % 4;
+            }
+        }
+    }
+}
---
+++ Algorithms/Algorithms/Tests/MatrixInSpiralTests.cs
@@ -0,0 +1,23 @@
+﻿using Algorithms.Sources;
+
+using NUnit.Framework;
+
+namespace Algorithms.Tests
+{
+    [TestFixture]
+    public class MatrixInSpiralTests
+    {
+        [Test]
+        public void Test()
+        {
+            var matrix = new int[,]
+            {
+                {1, 2, 3, 4, 5},
+                {6, 7, 8, 9, 10},
+                {11, 12, 13, 14, 15},
+                {16, 17, 18, 19, 20}
+            };
+            MatrixInSpiral.Traverse(matrix, 4, 5);
+        }
+    }
+}
